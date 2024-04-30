1192b582826934c38ec3b0d07db6f80ee760d900
Gabriel Colle <gabrielcolle3@hotmail.com>
2024-04-30 19:00:28 -0300

    adding comments

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,39 +1,49 @@
+// Importing required libraries
 const express = require("express");
 const axios = require("axios");
 const cors = require("cors");
 const { JSDOM } = require("jsdom");
 
+// Initializing the Express app
 const app = express();
 app.use(cors());
 const PORT = 5000;
 
+// Function to remove unnecessary CSS styles
 function removeCSS(html) {
   return html.replace(/<style\b[^<]*(?:(?!<\/style>)<[^<]*)*<\/style>/gi, "");
 }
 
+// GET route to access scraping data
 app.get("/api/scrape", async (req, res) => {
   const { keyword } = req.query;
 
+  // Checking if the keyword is present in the request
   if (!keyword) return res.status(400).json({ message: "Missing keyword" });
 
+  // Fetching data from the Amazon website based on the keyword
   try {
     const { data } = await axios.get(
       `https://www.amazon.com/s?k=${encodeURIComponent(keyword)}`,
       {
+        // Adding headers to avoid 503 errors
         headers: {
           "User-Agent":
             "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3",
         },
       }
     );
+    // Removing unnecessary CSS styles from the data
     const filteredHTML = removeCSS(data);
-
+    // Creating a new JSDOM object for data manipulation
     const dom = new JSDOM(filteredHTML);
     const products = [];
 
+    // Iterating over page elements to get product data
     dom.window.document.querySelectorAll(".s-result-item").forEach((item) => {
       const titleElement = item.querySelector(".a-text-normal");
       const image = item.querySelector("img")?.getAttribute("src");
+      // Checking for available title and image
       if (!titleElement || !image) {
         return;
       }
@@ -47,18 +57,22 @@ app.get("/api/scrape", async (req, res) => {
       const numberReviewsText = numberReviewsElement
         ? numberReviewsElement.textContent.trim()
         : "N/A";
+      // Converting number of reviews to numeric format
       const numberReviews = parseInt(numberReviewsText, 10) || "N/A";
       const product = { title, rating, numberReviews, image };
       products.push(product);
       console.log(product);
     });
 
+    // Returning product data in JSON format
     res.json(products);
   } catch (err) {
+    // Handling errors during scraping process
     return res.status(500).json({ message: err.message });
   }
 });
 
+// Starting the server on the specified port
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
--- frontend/script.js
+++ frontend/script.js
@@ -1,47 +1,62 @@
+// Function to perform Amazon product search
 async function searchAmazon() {
+  // Get keyword from input field and trim whitespace
   const keyword = document.getElementById("keywordInput").value.trim();
+  // Get result container element
   const resultContainer = document.getElementById("results");
 
+  // Check if keyword is empty
   if (!keyword) {
     alert("Please enter a keyword");
     return;
   }
 
   try {
+    // Fetch data from API based on keyword
     const response = await fetch(
       `http://localhost:5000/api/scrape?keyword=${encodeURIComponent(keyword)}`
     );
 
+    // Check if response is successful
     if (!response.ok) {
       throw new Error("Request failed");
     }
 
+    // Parse response data as JSON
     const data = await response.json();
 
+    // Clear previous content in result container
     resultContainer.innerHTML = "";
 
+    // Display message if no results found
     if (data.length === 0) {
       resultContainer.textContent = "No results found.";
       return;
     }
 
+    // Iterate through each product in the data
     data.forEach((product) => {
+      // Create a new div element for each product
       const div = document.createElement("div");
       div.classList.add("box");
 
+      // Create box content container
       const boxContent = document.createElement("div");
       boxContent.classList.add("box-content");
       div.appendChild(boxContent);
 
+      // Create and append product image
       const img = document.createElement("img");
       img.classList.add("box-img");
       img.src = product.image;
       boxContent.appendChild(img);
 
+      // Create and append product title with truncation and hover effect
       const title = document.createElement("h2");
       title.classList.add("box-title");
       title.textContent = truncateText(product.title, 50);
       title.title = product.title;
+      // adding EventListener for hover effect on title (title is big)
       title.addEventListener("mouseover", function () {
         this.textContent = this.title;
       });
@@ -50,19 +65,23 @@ async function searchAmazon() {
       });
       boxContent.appendChild(title);
 
+      // Create and append product rating and reviews information
       const ratingReviews = document.createElement("h2");
       ratingReviews.classList.add("box-RatingReviews");
       ratingReviews.textContent = `Rating: ${product.rating} - Reviews: ${product.numberReviews}`;
       boxContent.appendChild(ratingReviews);
 
+      // Append the product div to the result container
       resultContainer.appendChild(div);
     });
   } catch (error) {
+    // Log and display error if data fetching fails
     console.error("Error fetching data:", error);
     resultContainer.innerHTML = "An error occurred while fetching data.";
   }
 }
 
+// Function to truncate text based on maximum length
 function truncateText(text, maxLength) {
   if (text.length > maxLength) {
     return text.slice(0, maxLength) + "...";
